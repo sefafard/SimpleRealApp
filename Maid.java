683f93d9704545b3b6e82b8d6866aca55892cecc
Steven Fafard <sfafard@rocketsoftware.com>
2018-05-09 08:45:33 -0700

    I think this is good for now.

--- Maid.java
+++ Maid.java
@@ -1,17 +1,20 @@
 public class Maid extends Employee implements primWork {
 
-    static int workHrsPerWeek = 37;
-    static int breakHrsPerWeek = 8;
+    static int maidWorkHrsPerWeek = 37;
+    static int maidBreakHrsPerWeek = 8;
 
 //constructor
-     Maid (String job) {
-    super (job);
+ Maid (String job, int workHrsPerWeek, int breakHrsPerWeek) {
+    super (job, maidWorkHrsPerWeek, maidBreakHrsPerWeek);
+    workHrsPerWeek = maidWorkHrsPerWeek;
+    breakHrsPerWeek = maidBreakHrsPerWeek;
+    // System.out.printf("Maid constructor for job %s with hrs/wk = %d and breakhrs = %d\n", job, workHrsPerWeek, breakHrsPerWeek);
 }
 // methods
 
     public void primaryWork() {
         System.out.println("My job is:  "+ job +".");
-        System.out.println("I primarily supervise others' work.");
-        System.out.println("workHrsPerWeek value ="+workHrsPerWeek+" and break is "+breakHrsPerWeek);
+        System.out.println("I clean folks' rooms.");
+        // System.out.println("I work aproximately "+workHrsPerWeek+" hours per week and am allowed breaks for up to "+breakHrsPerWeek+ " hours per week.");
     }
 }
